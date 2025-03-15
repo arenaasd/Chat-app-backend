3ad4f2c7fcade8b850eb80ee7e5a2746575361c9
arenaasd <akgamerz397@gmail.com>
2025-03-16 02:52:00 +0500

    first commit

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 import express from "express";
-import auth from "./routes/auth.js";
+import auth from "./src/routes/auth.js";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
+import { connectDB } from "./src/config/db.js";
 import cookieParser from "cookie-parser";
-import message from "./routes/message.js";
+import message from "./src/routes/message.js";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { app , server } from "./config/socket.js";
+import { app , server } from "./src/config/socket.js";
 
 
 app.use(express.json({ limit: "10mb" })); 
 app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
 dotenv.config();
 app.use(cookieParser())
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+const frontendUrl = process.env.FRONTEND_URL || "http://localhost:5173";
 
+// Set up CORS with the dynamic frontend URL
+app.use(cors({
+    origin: frontendUrl,  // Use FRONTEND_URL or fallback to localhost:5173
+    credentials: true  // Allow credentials (cookies, authorization headers)
+}));
 
 app.use("/api/auth", auth)
 app.use("/api/messages", message)
