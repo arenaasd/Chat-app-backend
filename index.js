027bf1d1e2265b8d987ccc83597a6c218248a376
Arsalankhan1218 <90974742+arenaasd@users.noreply.github.com>
2025-03-16 20:28:10 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { app , server } from "./src/config/socket.js";
 
+connectDB();
+
 
 app.use(express.json({ limit: "10mb" })); 
 app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
 dotenv.config();
 app.use(cookieParser())
-const frontendUrl = process.env.FRONTEND_URL || "http://localhost:5173";
+
 
 // Set up CORS with the dynamic frontend URL
 app.use(cors({
-    origin: frontendUrl,  // Use FRONTEND_URL or fallback to localhost:5173
+    origin: process.env.FRONTEND_URL ,  // Use FRONTEND_URL or fallback to localhost:5173
     credentials: true  // Allow credentials (cookies, authorization headers)
 }));
 
@@ -29,6 +31,5 @@ app.use("/api/messages", message)
 const port = process.env.PORT
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-    connectDB();
 });
 
