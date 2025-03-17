40a0f6ce74ddd26d6276ad8633f48439d560e38c
Arsalankhan1218 <90974742+arenaasd@users.noreply.github.com>
2025-03-17 05:23:43 +0500

    Update socket.js

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,22 +1,27 @@
+// In your socket.js file
+
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 
+// Define app and server
 const app = express();
 const server = http.createServer(app);
 
+// Initialize io (ensure this is initialized before using it)
 const io = new Server(server, {
   cors: {
     origin: [process.env.FRONTEND_URL],
   },
 });
 
+// used to store online users
+const userSocketMap = {}; // {userId: socketId}
+
+// Export the function to get the receiver socket ID
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
-
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
@@ -24,7 +29,6 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
@@ -34,4 +38,5 @@ io.on("connection", (socket) => {
   });
 });
 
+// Export io, app, and server so other files can use them
 export { io, app, server };
